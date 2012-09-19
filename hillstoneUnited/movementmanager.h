da2b894b77c6004526a3c1a6fea42165f03f362a
ganow <y.nagano.92@gmail.com>
2012-09-19 14:54:06 +0900

    fix mm
    I fixed mm

--- hillstoneUnited/movementmanager.h
+++ hillstoneUnited/movementmanager.h
@@ -4,6 +4,21 @@
 #include <iostream>
 #include "world.h"
 #include "util.h"
+#include "movement/forward.h"
+#include "movement/slowforward.h"
+#include "movement/dribble.h"
+#include "movement/ldribble.h"
+#include "movement/rdribble.h"
+#include "movement/ldribblesharp.h"
+#include "movement/rdribblesharp.h"
+#include "movement/back.h"
+#include "movement/slowback.h"
+#include "movement/tleft.h"
+#include "movement/tright.h"
+#include "movement/tleftin.h"
+#include "movement/trightin.h"
+#include "movement/sleft.h"
+#include "movement/sright.h"
 #include "movement/dummy.h"
 #include "movement/standup.h"
 #include "movement/ffwalk.h"
@@ -17,6 +32,21 @@
       std::string nextStr(Action& act, World& w);
 
     private:
+      Forward forward;
+      SlowForward slowForward;
+      Dribble dribble;
+      LDribble lDribble;
+      RDribble rDribble;
+      LDribbleSharp lDribbleSharp;
+      RDribbleSharp rDribbleSharp;
+      Back back;
+      SlowBack slowBack;
+      TLeft tLeft;
+      TRight tRight;
+      TLeftIn tLeftIn;
+      TRightIn tRightIn;
+      SLeft sLeft;
+      SRight sRight;
       Dummy dummy;
       Standup standup;
       FFWalk ffWalk;
